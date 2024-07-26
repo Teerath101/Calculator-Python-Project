4f1fbe4e960eeb9f53a27a64c398592b886c27d4
Teerath101 <101teerath@gmail.com>
2024-07-25 21:07:29 -0400

    Adding buttons but receiving some errors

--- Calculator.py
+++ Calculator.py
@@ -9,6 +9,32 @@ root.geometry("358x500+600+200")
 root.resizable(False, False)
 root.configure(bg="#17161b")
 
+label_result= Label(root, width=25, height=2, text="", font=("arial",30))
+label_result.pack()
+
+Button(root,text="C", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=10,y=100)
+Button(root,text="/", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=100,y=100)
+Button(root,text="%", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=200,y=100)
+Button(root,text="*", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=300,y=100)
+
+
+Button(root,text="7", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=10,y=200)
+Button(root,text="8", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=100,y=200)
+Button(root,text="9", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=200,y=200)
+Button(root,text="-", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=300,y=200)
+
+
+Button(root,text="4", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=10,y=300)
+Button(root,text="5", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=100,y=300)
+Button(root,text="6", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=200,y=300)
+Button(root,text="+", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=300,y=300)
+
+Button(root,text="1", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=10,y=400)
+Button(root,text="2", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=100,y=400)
+Button(root,text="3", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=200,y=400)
+Button(root,text="0", width=9, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=300,y=500)
+
+
 
 
 #Running the application
