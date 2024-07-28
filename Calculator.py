0a259b15328236ddbbbcecac328380f0bb16a008
Teerath101 <101teerath@gmail.com>
2024-07-28 03:50:28 -0400

    Tkinter tk and ttk not working on Mac

--- Calculator.py
+++ Calculator.py
@@ -1,42 +1,23 @@
-# Importing Tkinter module and * from Tkinter
-import tkinter
-from tkinter import *
+import tkinter as tk
+from tkinter import ttk
 
-#Setting up main application window
-root = Tk()
-root.title("First P Calculator")
-root.geometry("358x500+600+200")
-root.resizable(False, False)
-root.configure(bg="#17161b")
+main_window = tk.Tk()
+main_window.title("Tkinter.Ttk Widgets Example")
 
-label_result= Label(root, width=25, height=2, text="", font=("arial",30))
-label_result.pack()
+# Create a label widget
+label = tk.Label(main_window, text="This is a Tkinter.Ttk Label")
+label.pack()
 
-Button(root,text="C", width=2, height=3, font=("arial",30,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=10, y=100)
-Button(root,text="/", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=100,y=100)
-Button(root,text="%", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=200,y=100)
-Button(root,text="*", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=300,y=100)
+# Create an entry widget
+entry = tk.Entry(main_window)
+entry.pack()
 
-
-Button(root,text="7", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=10,y=200)
-Button(root,text="8", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=100,y=200)
-Button(root,text="9", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=200,y=200)
-Button(root,text="-", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=300,y=200)
-
-
-Button(root,text="4", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=10,y=300)
-Button(root,text="5", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=100,y=300)
-Button(root,text="6", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=200,y=300)
-Button(root,text="+", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=300,y=300)
-
-Button(root,text="1", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=10,y=400)
-Button(root,text="2", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=100,y=400)
-Button(root,text="3", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=200,y=400)
-Button(root,text="0", width=4, height=1, font=("arial",15,"bold"), bd=1,fg="#fff",bg="#3697f5").place(x=300,y=400)
+# Create a button widget
+button = tk.Button(main_window, text="Click Me")
+button.pack()
 
 
 
 
 
-#Running the application
-root.mainloop()
+main_window.mainloop()
